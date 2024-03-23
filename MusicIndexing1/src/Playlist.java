84d10fa250ed45d15afaf9c82d2d5db079c609bd
Khalid <114075378+khalidshittabey@users.noreply.github.com>
2024-03-23 20:17:12 +0000

    Add files via upload

---
+++ MusicIndexing1/src/Playlist.java
@@ -0,0 +1,10 @@
+
+import java.util.ArrayList;
+
+
+public class Playlist implements PlaylistInterface {
+    
+   
+    }
+    
+
---
+++ MusicIndexing1/src/Song.java
@@ -0,0 +1,18 @@
+
+public class Song {
+    
+    private String title;
+    private String artist;
+    private int duration;
+    private String genre;
+
+    public Song(String title, String artist, int duration, String genre) {
+        this.title = title;
+        this.artist = artist;
+        this.duration = duration;
+        this.genre = genre;
+    }
+
+   
+    }
+    
