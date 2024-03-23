905ab22d0f667849e6dd62143c695e17494753ce
Khalid <114075378+khalidshittabey@users.noreply.github.com>
2024-03-23 20:17:31 +0000

    Add files via upload

---
+++ MusicIndexing2/src/PlaylistLibrary.java
@@ -0,0 +1,15 @@
+
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+public class PlaylistLibrary extends JFrame {
+
+   
+    }
+
+    
+
